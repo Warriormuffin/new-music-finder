16c19dcb3bdf4edebbd6c3a5016939c80c685d9d
Nathan Elg <elgn@students.wwu.edu>
2017-04-26 10:46:11 -0600

    made songs appear on screen

--- app.js
+++ app.js
@@ -1,17 +1,42 @@
-function ItunesController(){
+function ItunesController() {
   var itunesService = new ItunesService()
   //Do Not Modify the getMusic function
-  this.getMusic = function getMusic(e){
+  this.getMusic = function getMusic(e) {
     e.preventDefault();
     var artist = e.target.artist.value;
     itunesService.getMusicByArtist(artist).then(drawSongs);
   }
 
-  function drawSongs(songList){
+  function drawSongs(songList) {
     console.log(songList);
-    // This is where you task begins
-
-
+    var draw = document.getElementById('songs');
+    var songTemplate = "";
+    debugger
+    for (var i = 0; i < songList.length; i++) {
+      var song = songList[i];
+      songTemplate += `
+        <div class="container">
+          <div class="row">
+            <div class="col-xs-12">
+              <div class="well">
+                <div class="col-xs-2">
+                  <img src ="${song.albumArt}">
+                </div class="col-xs-2">
+                  <h4>${song.artist}</h4>
+                <div class="col-xs-2">
+                  <h4>${song.collection}</h4>
+                </div class="col-xs-2">
+                  <h4>${song.title}</h4>
+                <div class="col-xs-2">
+                  <h4>${song.price}</h4>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        `
+    }
+    draw.innerHTML = songTemplate
 
   }
 
